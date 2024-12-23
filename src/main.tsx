35b97ca2e4a1bf878f0666d7cf6c6ecd234e760c
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-23 17:29:09 +0200

    + redux-toolkit and redux-sags basic setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Router from './Router.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </StrictMode>,
 )
---
+++ src/redux/store.ts
@@ -0,0 +1,23 @@
+import type { Action, ThunkAction } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './rootSaga'
+
+const sagaMiddleware = createSagaMiddleware()
+
+export const store = configureStore({
+  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+})
+
+sagaMiddleware.run(rootSaga)
+
+export type AppStore = typeof store
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
+export type AppThunk<ThunkReturnType = void> = ThunkAction<
+  ThunkReturnType,
+  RootState,
+  unknown,
+  Action
+>
