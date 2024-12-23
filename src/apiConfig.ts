df1e41742e038e3f4bbf2a591b9bd0d4024f300b
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-23 16:00:23 +0200

    add api config

---
+++ src/apiConfig.ts
@@ -0,0 +1,51 @@
+import axios, {
+  AxiosError,
+  InternalAxiosRequestConfig,
+  type CreateAxiosDefaults,
+} from 'axios';
+import { authService } from './services/authService';
+
+const options: CreateAxiosDefaults = {
+  baseURL: process.env.VITE_SERVER_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+const api = axios.create(options);
+const apiWithAuth = axios.create(options);
+
+apiWithAuth.interceptors.request.use(async (config) => {
+  const accessToken = authService.getAccessToken();
+
+  if (config?.headers && accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
+
+apiWithAuth.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (error: AxiosError) {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && error.config) {
+      if (originalRequest?.data) {
+        originalRequest.data._isRetry = true;
+      }
+      const token = (await authService.getNewTokens()).data;
+      if (token) authService.setAccessToken(token);
+      return apiWithAuth.request(
+        originalRequest as InternalAxiosRequestConfig<{ _isRetry: boolean }>,
+      );
+    }
+
+    return Promise.reject(error);
+  },
+);
+
+export { api, apiWithAuth };
