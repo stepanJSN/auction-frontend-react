0ce6b8977b1c1a4fe5fcd8ed02aaec445b89b8f4
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-24 15:18:32 +0200

    changed class export into export inline object

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,10 +1,8 @@
 import { api } from "../apiConfig";
 import { ICreateUser } from "../types/userService.interfaces";
 
-class UserService {
-  create = async (data: ICreateUser) => {
+export const userService = {
+  create: async (data: ICreateUser) => {
     await api.post('/users', data);
   }
 }
-
-export const userService = new UserService();
