6428fcf4fb2c94b4815a3f65e21794472ebbd201
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-25 10:22:54 +0200

    change eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,58 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "react-app",
+  ],
+  plugins: [
+    "react",
+    "react-refresh",
+    "@arthurgeron/react-usememo",
+  ],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-uses-react": "off",
+    "eol-last": ["error", "always"],
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "@arthurgeron/react-usememo/require-usememo": "warn",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: "detect",
+      createClass: "createReactClass",
+      pragma: "React",
+      fragment: "Fragment",
+      defaultVersion: "",
+      propWrapperFunctions: [
+        "forbidExtraProps",
+        { property: "freeze", object: "Object" },
+        { property: "myFavoriteWrapper" },
+        { property: "forbidExtraProps", exact: true },
+      ],
+      componentWrapperFunctions: [
+        "observer",
+        { property: "styled" },
+        { property: "observer", object: "Mobx" },
+        { property: "observer", object: "<pragma>" },
+      ],
+      formComponents: [
+        "CustomForm",
+        { name: "SimpleForm", formAttribute: "endpoint" },
+        { name: "Form", formAttribute: ["registerEndpoint", "loginEndpoint"] },
+      ],
+      linkComponents: [
+        "Hyperlink",
+        { name: "MyLink", linkAttribute: "to" },
+        { name: "Link", linkAttribute: ["to", "href"] },
+      ],
+    },
+  },
+};
