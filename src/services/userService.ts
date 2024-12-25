26d2ae6bcbc65c9bfbb454216eddc0a54d829454
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-25 13:49:31 +0200

    add get user method to user service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,8 +1,13 @@
-import { api } from "../apiConfig";
-import { ICreateUser } from "../types/userService.interfaces";
+import { api, apiWithAuth } from "../apiConfig";
+import { ICreateUser, IUser } from "../types/userService.interfaces";
 
 export const userService = {
   create: async (data: ICreateUser) => {
     await api.post('/users', data);
+  },
+
+  getOne: async (id: string) => {
+    const userData = await apiWithAuth.get<IUser>(`/users/${id}`);
+    return userData.data;
   }
 }
