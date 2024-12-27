c9025b8700205428ce98e2d4a6909101f57c7dc0
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-27 11:22:49 +0200

    add user cards saga to root saga

--- src/redux/rootSaga.ts
+++ src/redux/rootSaga.ts
@@ -1,7 +1,12 @@
 import { all, fork } from 'redux-saga/effects';
 import { watchAuthSaga } from '../features/auth/authSaga';
 import { watchUserSaga } from '../features/users/userSaga';
+import { watchUserCardsSaga } from '../features/userCards/userCardsSaga';
 
 export default function* rootSaga() {
-  yield all([fork(watchAuthSaga), fork(watchUserSaga)]);
+  yield all([
+    fork(watchAuthSaga),
+    fork(watchUserSaga),
+    fork(watchUserCardsSaga),
+  ]);
 }
