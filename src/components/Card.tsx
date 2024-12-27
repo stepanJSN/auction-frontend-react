dd6016be9f59c559313ce6f64f6ebed6fc8195e4
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-27 11:23:16 +0200

    update styles of card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,25 @@
+import { useMemo } from 'react';
 import {
+  Button,
+  CardActions,
   CardContent,
   CardMedia,
   Card as MuiCard,
+  Stack,
   Typography,
 } from '@mui/material';
 import { ICardSummary } from '../types/cards.interface';
+import { CardLabel } from './CardLabel';
 
 type CardProps = ICardSummary;
+
+const cardMediaStyles = {
+  height: '200px',
+};
+
+const cardContentStyles = {
+  flex: 'auto',
+};
 
 export default function Card({
   name,
@@ -16,17 +29,34 @@ export default function Card({
   is_active,
   type,
 }: CardProps) {
+  const cardStyles = useMemo(
+    () => ({
+      height: '100%',
+      backgroundColor: is_active ? '' : 'grey.300',
+    }),
+    [is_active],
+  );
   return (
-    <MuiCard>
-      <CardMedia image={image_url} title={name} />
-      {is_created_by_admin && (
-        <Typography>Card was created by admin</Typography>
-      )}
-      <CardContent>
-        <Typography variant="h5">{name}</Typography>
-        <Typography variant="body1">Gender: {gender}</Typography>
-        <Typography variant="body1">Type: {type}</Typography>
-      </CardContent>
+    <MuiCard sx={cardStyles}>
+      <Stack height="100%">
+        <CardMedia sx={cardMediaStyles} image={image_url} title={name} />
+        {is_created_by_admin && (
+          <CardLabel>Card was created by admin</CardLabel>
+        )}
+        <CardContent sx={cardContentStyles}>
+          <Typography variant="h5" gutterBottom>
+            {name}
+          </Typography>
+          <Typography variant="body1">Gender: {gender}</Typography>
+          {type && <Typography variant="body1">Type: {type}</Typography>}
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+          <Button size="small" color="success">
+            Sell
+          </Button>
+        </CardActions>
+      </Stack>
     </MuiCard>
   );
 }
