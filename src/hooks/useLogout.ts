ad23e108d4ac3fed6acb549699b4d16746bb2661
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-28 13:00:41 +0200

    reset of the entire store on logout

--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -2,15 +2,15 @@ import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router';
 import { AppDispatch } from '../redux/store';
-import { logout as logoutAction } from '../features/auth/authSlice';
 import { ROUTES } from '../config/routesConfig';
+import { RESET_ACTION } from '../redux/rootReducer';
 
 export default function useLogout() {
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
 
   const logout = useCallback(() => {
-    dispatch(logoutAction());
+    dispatch({ type: RESET_ACTION });
     navigate(ROUTES.SIGN_IN);
   }, [dispatch, navigate]);
 
