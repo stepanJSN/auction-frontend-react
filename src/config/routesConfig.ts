34d06ad266eb6defea54854bfa329d7c3d7c8a83
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-28 11:56:44 +0200

    extract routes to separate file

---
+++ src/config/routesConfig.ts
@@ -0,0 +1,13 @@
+export const ROUTES = {
+  SIGN_UP: '/signup',
+  SIGN_IN: '/signin',
+  USER_CARDS: '/',
+  USER_CARDS_DETAILS: '/:cardId',
+  PROFILE: '/profile',
+  AUCTIONS: '/auctions',
+  CHATS: '/chats',
+  CARDS: '/cards',
+  SETS: '/sets',
+  STATISTICS: '/statistics',
+  USERS: '/users',
+};
