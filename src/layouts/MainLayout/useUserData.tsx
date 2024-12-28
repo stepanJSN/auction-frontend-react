f0347c2cecd412636765e9e77de4d2dc45aa851b
Stepan JSN <stepan.s.jsn@gmail.com>
2024-12-28 14:36:30 +0200

    update main layout

---
+++ src/layouts/MainLayout/useUserData.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { authService } from '../../services/authService';
+import { signinSuccess } from '../../features/auth/authSlice';
+import { Role } from '../../enums/role.enum';
+
+export default function useUserData() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const userId = authService.getUserId();
+    const userRole = authService.getUserRole() as Role;
+    if (userId && userRole) {
+      dispatch(signinSuccess({ id: userId, role: userRole }));
+    }
+  }, [dispatch]);
+}
