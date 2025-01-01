ff8857dc4020454cafa3b2a1dd7c486d6622adfa
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-01 16:48:01 +0200

    custom switch and select components

---
+++ src/components/Switch.tsx
@@ -0,0 +1,18 @@
+import { useMemo } from 'react';
+import { FormControlLabel, Switch as MuiSwitch } from '@mui/material';
+
+type SwitchProps = {
+  label: string;
+  checked: boolean;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function Switch({ checked, handleChange, label }: SwitchProps) {
+  const control = useMemo(
+    () => (
+      <MuiSwitch defaultChecked checked={checked} onChange={handleChange} />
+    ),
+    [checked, handleChange],
+  );
+  return <FormControlLabel control={control} label={label} />;
+}
