1d2eb1c26220ebe424fbda6584f1bb957d82a2c1
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-02 11:52:09 +0200

    add manage and sell buttons on cards page

--- src/pages/AllCardsPage.tsx
+++ src/pages/AllCardsPage.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-import { Grid2, Typography } from '@mui/material';
+import { Button, Grid2, Typography } from '@mui/material';
 import Card from '../components/Card';
 import { QueryStatusEnum } from '../enums/queryStatus.enum';
 import Pagination from '../components/Pagination';
@@ -14,11 +14,14 @@ import {
 import FaqHeader from '../components/FaqHeader';
 import { ROUTES } from '../config/routesConfig';
 import usePaginatedData from '../hooks/usePaginatedData';
+import { selectAuth } from '../features/auth/authSlice';
+import { Role } from '../enums/role.enum';
 
 const cardColumnsNumber = { xs: 12, sm: 6, md: 4, lg: 3 };
 
 export default function AllCardsPage() {
   const { status, cards, currentPage, totalPages } = useSelector(selectCards);
+  const { role } = useSelector(selectAuth);
   const handlePageChange = usePaginatedData(
     currentPage,
     getCards,
@@ -35,7 +38,16 @@ export default function AllCardsPage() {
           <Grid2 container spacing={2}>
             {cards.map((card) => (
               <Grid2 size={cardColumnsNumber} key={card.id}>
-                <Card {...card} />
+                <Card {...card}>
+                  {role === Role.ADMIN && (
+                    <>
+                      <Button size="small" color="success">
+                        Sell
+                      </Button>
+                      <Button size="small">Manage</Button>
+                    </>
+                  )}
+                </Card>
               </Grid2>
             ))}
           </Grid2>
