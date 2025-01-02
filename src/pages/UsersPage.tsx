71078c27de35b0c26e029481642f58d7654a2e07
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-02 11:00:59 +0200

    move business logic to custom hook, create separate component for filters

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,159 +1,48 @@
 import {
   Button,
-  Grid2,
-  GridSize,
   LinearProgress,
-  SelectChangeEvent,
   Stack,
   SxProps,
   Typography,
 } from '@mui/material';
 import UsersTable from '../features/users/UsersTable';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  changeFilterParams,
-  deleteUser,
-  getUsers,
-  selectUsers,
-  updateUserRole,
-} from '../features/users/usersSlice';
-import { useEffect, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { selectUsers } from '../features/users/usersSlice';
 import { QueryStatusEnum } from '../enums/queryStatus.enum';
 import PageError from '../components/PageError';
 import PageLoader from '../components/PageLoader';
-import { Role } from '../enums/role.enum';
-import BasicSelect from '../components/Select';
-import { UsersSortTypeEnum } from '../types/user.interfaces';
-import { SortOrderEnum } from '../enums/sortOrder.enum';
-import Switch from '../components/Switch';
+import useUsersLogic from '../features/users/useUsersLogic';
+import UsersFilters from '../features/users/UsersFilters';
 
 const buttonContainerStyles: SxProps = {
   mt: 2,
 };
 
-const filterContainerStyles: SxProps = {
-  mb: 2,
-};
-
-const sortByOptions = [
-  { value: UsersSortTypeEnum.CREATION_DATE, label: 'Creation date' },
-  { value: UsersSortTypeEnum.RATING, label: 'Rating' },
-];
-
-const sortOrderOptions = [
-  { value: SortOrderEnum.ASC, label: 'Ascending' },
-  { value: SortOrderEnum.DESC, label: 'Descending' },
-];
-
-const sortColumnsBreakpoints: Record<string, GridSize> = {
-  // xs: 1,
-  sm: 'auto',
-  md: 3,
-};
-
 export default function UsersPage() {
+  const { status, users, hasMore, sortType, sortOrder, showOnlyAdmins } =
+    useSelector(selectUsers);
   const {
-    status,
-    users,
-    hasMore,
-    currentPage,
-    sortType,
-    sortOrder,
-    showOnlyAdmins,
-  } = useSelector(selectUsers);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getUsers());
-  }, [dispatch]);
-
-  const handleLoadMore = useCallback(() => {
-    dispatch(getUsers(currentPage + 1));
-  }, [currentPage, dispatch]);
-
-  const handleDelete = useCallback(
-    (userId: string) => {
-      dispatch(deleteUser(userId));
-    },
-    [dispatch],
-  );
-
-  const handleUpdate = useCallback(
-    (userId: string, role: Role) => {
-      dispatch(updateUserRole({ id: userId, role }));
-    },
-    [dispatch],
-  );
-
-  const handleSortTypeChange = useCallback(
-    (event: SelectChangeEvent) => {
-      dispatch(
-        changeFilterParams({
-          sortType: event.target.value as UsersSortTypeEnum,
-          sortOrder,
-          showOnlyAdmins,
-        }),
-      );
-    },
-    [dispatch, sortOrder, showOnlyAdmins],
-  );
-
-  const handleSortOrderChange = useCallback(
-    (event: SelectChangeEvent) => {
-      dispatch(
-        changeFilterParams({
-          sortType,
-          sortOrder: event.target.value as SortOrderEnum,
-          showOnlyAdmins,
-        }),
-      );
-    },
-    [dispatch, sortType, showOnlyAdmins],
-  );
-
-  const handleShowOnlyAdminsChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      dispatch(
-        changeFilterParams({
-          sortType,
-          sortOrder,
-          showOnlyAdmins: event.target.checked,
-        }),
-      );
-    },
-    [dispatch, sortType, sortOrder],
-  );
+    handleDelete,
+    handleUpdate,
+    handleLoadMore,
+    handleSortTypeChange,
+    handleSortOrderChange,
+    handleShowOnlyAdminsChange,
+  } = useUsersLogic();
 
   return (
     <>
       <Typography variant="h4" gutterBottom>
         Users
       </Typography>
-      <Grid2 spacing={1} sx={filterContainerStyles} container>
-        <Grid2 size={sortColumnsBreakpoints}>
-          <BasicSelect
-            label="Sort by"
-            value={sortType}
-            options={sortByOptions}
-            handleChange={handleSortTypeChange}
-          />
-        </Grid2>
-        <Grid2 size={sortColumnsBreakpoints}>
-          <BasicSelect
-            label="Sort order"
-            value={sortOrder}
-            options={sortOrderOptions}
-            handleChange={handleSortOrderChange}
-          />
-        </Grid2>
-        <Grid2>
-          <Switch
-            label="Show only admins"
-            checked={showOnlyAdmins}
-            handleChange={handleShowOnlyAdminsChange}
-          />
-        </Grid2>
-      </Grid2>
+      <UsersFilters
+        sortType={sortType}
+        sortOrder={sortOrder}
+        showOnlyAdmins={showOnlyAdmins}
+        handleSortTypeChange={handleSortTypeChange}
+        handleSortOrderChange={handleSortOrderChange}
+        handleShowOnlyAdminsChange={handleShowOnlyAdminsChange}
+      />
       {status === QueryStatusEnum.LOADING && users.length === 0 && (
         <PageLoader />
       )}
