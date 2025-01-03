6df5e9cee6157a825c80ed9ecec41dc56468813f
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-03 12:48:09 +0200

    add previous image url to use image hook

--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -1,8 +1,16 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
-export default function useImage() {
-  const [image, setImage] = useState<{ url: string; image: Blob } | null>(null);
+export default function useImage(previousImageUrl?: string) {
+  const [image, setImage] = useState<{ url: string; image?: Blob } | null>(
+    null,
+  );
   const [isImageError, setIsImageError] = useState(false);
+
+  useEffect(() => {
+    if (previousImageUrl) {
+      setImage({ url: previousImageUrl });
+    }
+  }, [previousImageUrl]);
 
   const handleUpload = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files;
