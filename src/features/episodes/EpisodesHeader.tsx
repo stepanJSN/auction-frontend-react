b3ef73f14af6fb02d35712b552eaaa70f37baa40
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-06 15:29:46 +0200

    adapt locations and episodes header

--- src/features/episodes/EpisodesHeader.tsx
+++ src/features/episodes/EpisodesHeader.tsx
@@ -2,6 +2,7 @@ import { Stack, Button, SxProps } from '@mui/material';
 import { Link } from 'react-router';
 import { ROUTES } from '../../config/routesConfig';
 import DebouncedInput from '../../components/DebouncedInput';
+import { ResponsiveStyleValue } from '@mui/system';
 
 type EpisodeHeaderProps = {
   handleFilterEpisodesByName: (name: string) => void;
@@ -11,11 +12,21 @@ const episodesHeaderStyles: SxProps = {
   mb: 2,
 };
 
+const episodesHeaderFlexDirection: ResponsiveStyleValue<
+  'row' | 'row-reverse' | 'column' | 'column-reverse'
+> = {
+  xs: 'column',
+  sm: 'row',
+};
+
 export default function EpisodesHeader({
   handleFilterEpisodesByName,
 }: EpisodeHeaderProps) {
   return (
-    <Stack direction="row" spacing={2} sx={episodesHeaderStyles}>
+    <Stack
+      direction={episodesHeaderFlexDirection}
+      spacing={1}
+      sx={episodesHeaderStyles}>
       <DebouncedInput
         label="Episode name"
         handleDebouncedChange={handleFilterEpisodesByName}
--- src/features/locations/LocationsHeader.tsx
+++ src/features/locations/LocationsHeader.tsx
@@ -2,6 +2,7 @@ import { Stack, Button, SxProps } from '@mui/material';
 import { Link } from 'react-router';
 import { ROUTES } from '../../config/routesConfig';
 import DebouncedInput from '../../components/DebouncedInput';
+import { ResponsiveStyleValue } from '@mui/system';
 
 type LocationHeaderProps = {
   handleFilterLocationsByName: (name: string) => void;
@@ -11,11 +12,21 @@ const locationsHeaderStyles: SxProps = {
   mb: 2,
 };
 
+const locationHeaderFlexDirection: ResponsiveStyleValue<
+  'row' | 'row-reverse' | 'column' | 'column-reverse'
+> = {
+  xs: 'column',
+  sm: 'row',
+};
+
 export default function LocationsHeader({
   handleFilterLocationsByName,
 }: LocationHeaderProps) {
   return (
-    <Stack direction="row" spacing={2} sx={locationsHeaderStyles}>
+    <Stack
+      direction={locationHeaderFlexDirection}
+      spacing={1}
+      sx={locationsHeaderStyles}>
       <DebouncedInput
         label="Location name"
         handleDebouncedChange={handleFilterLocationsByName}
