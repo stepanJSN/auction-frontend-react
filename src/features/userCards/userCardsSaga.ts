ef0af7e337e47e394f5ede2e48231b54a3299059
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-07 18:01:50 +0200

    refactor user cards slice and saga

--- src/features/userCards/userCardsSaga.ts
+++ src/features/userCards/userCardsSaga.ts
@@ -1,20 +1,10 @@
-import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { cardsService } from '../../services/cardsService';
 import { IGetCardsResponse } from '../../types/cards.interface';
-import {
-  getCards,
-  getCardsError,
-  getCardsSuccess,
-  selectIsPageLoaded,
-} from './userCardsSlice';
+import { getCards, getCardsError, getCardsSuccess } from './userCardsSlice';
 import { PayloadAction } from '@reduxjs/toolkit';
 
 function* getUserCardsSaga(action: PayloadAction<number>) {
-  const isPageLoaded: boolean = yield select(selectIsPageLoaded);
-  if (isPageLoaded) {
-    yield put(getCardsSuccess(null));
-    return;
-  }
   try {
     const userCards: IGetCardsResponse = yield call(cardsService.getAll, {
       page: action.payload,
