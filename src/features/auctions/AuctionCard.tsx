e626c01b16acaf0ff0d79166334904ecd9364ac8
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-08 12:16:23 +0200

    update auction card ui

--- src/features/auctions/AuctionCard.tsx
+++ src/features/auctions/AuctionCard.tsx
@@ -5,10 +5,12 @@ import {
   CardContent,
   CardMedia,
   Stack,
+  SxProps,
   Typography,
 } from '@mui/material';
 import { CardLabel } from '../../components/CardLabel';
 import { Link } from 'react-router';
+import dayjs from 'dayjs';
 
 type AuctionCardProps = {
   cardName: string;
@@ -16,6 +18,22 @@ type AuctionCardProps = {
   isUserLeader: boolean;
   endTime: string;
   highestBid: number | null;
+  minBidStep: number;
+  startingBid: number;
+  maxBid?: number;
+  isCompleted?: boolean;
+};
+
+const cardWrapperStyles: SxProps = {
+  height: '100%',
+};
+
+const cardMediaStyles: SxProps = {
+  height: '200px',
+};
+
+const cardContentStyles = {
+  flex: 'auto',
 };
 
 export default function AuctionCard({
@@ -24,23 +42,35 @@ export default function AuctionCard({
   isUserLeader,
   endTime,
   highestBid,
+  maxBid,
+  startingBid,
+  minBidStep,
+  isCompleted,
 }: AuctionCardProps) {
   return (
-    <Card>
-      <Stack>
-        <CardMedia image={imageUrl} title={cardName} />
+    <Card sx={cardWrapperStyles}>
+      <Stack sx={cardWrapperStyles}>
+        <CardMedia sx={cardMediaStyles} image={imageUrl} title={cardName} />
         {isUserLeader && (
           <CardLabel colorVariant="success">Your bid is the highest</CardLabel>
         )}
-        <CardContent>
+        {isCompleted && (
+          <CardLabel colorVariant="success">Auction is completed</CardLabel>
+        )}
+        <CardContent sx={cardContentStyles}>
           <Typography variant="h5" gutterBottom>
             {cardName}
           </Typography>
-          <Typography>{endTime}</Typography>
-          <Typography>{highestBid}</Typography>
+          <Typography>
+            End time: {dayjs(endTime).format('YYYY-MM-DD HH:mm')}
+          </Typography>
+          <Typography>Highest bid: {highestBid ?? 'None'}</Typography>
+          {startingBid && <Typography>Starting bid: {startingBid}</Typography>}
+          <Typography>Min bid step: {minBidStep}</Typography>
+          {maxBid && <Typography>Max bid: {maxBid}</Typography>}
         </CardContent>
         <CardActions>
-          <Button variant="contained" component={Link} to="">
+          <Button fullWidth variant="contained" component={Link} to="">
             Make bid
           </Button>
         </CardActions>
