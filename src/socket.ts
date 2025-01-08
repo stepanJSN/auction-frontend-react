cb686890033ab0da26fd33678075d06ea6dd648a
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-08 18:05:13 +0200

    add auth token to websockets and change transport type

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,12 @@
 import { io } from 'socket.io-client';
+import { authService } from './services/authService';
 
 const URL = import.meta.env.VITE_SERVER_URL;
+const accessToken = authService.getAccessToken();
 
-export const socket = io(URL);
+export const socket = io(URL, {
+  auth: {
+    token: `Bearer ${accessToken}`,
+  },
+  transports: ['websocket'],
+});
