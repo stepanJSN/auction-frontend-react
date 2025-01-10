0f357be58583ce9fedfcb811078b0f158596797c
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-10 11:53:10 +0200

    navigate to auctions list page when delete is successful

--- src/features/auctions/editAuction/useDeleteAuction.tsx
+++ src/features/auctions/editAuction/useDeleteAuction.tsx
@@ -1,6 +1,8 @@
 import { useCallback } from 'react';
 import useMutation from '../../../hooks/useMutation';
 import { auctionService } from '../../../services/auctionService';
+import { useNavigate } from 'react-router';
+import { ROUTES } from '../../../config/routesConfig';
 
 export default function useDeleteAuction(auctionId?: string) {
   const {
@@ -10,11 +12,13 @@ export default function useDeleteAuction(auctionId?: string) {
   } = useMutation((auctionId: string) => {
     return auctionService.delete(auctionId);
   });
+  const navigate = useNavigate();
 
   const handleDelete = useCallback(() => {
     if (!auctionId) return;
     mutate(auctionId);
-  }, [auctionId, mutate]);
+    navigate(ROUTES.AUCTIONS);
+  }, [auctionId, mutate, navigate]);
 
   return { handleDelete, deleteStatus, errorCode };
 }
