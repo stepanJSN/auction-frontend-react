4cce41ea92e677248a3248a781753c1c3891432f
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-13 13:05:35 +0200

    extract logic of chats page to hook

--- src/pages/AllChatsPage.tsx
+++ src/pages/AllChatsPage.tsx
@@ -6,18 +6,8 @@ import {
   Typography,
 } from '@mui/material';
 import DebouncedInput from '../components/DebouncedInput';
-import { useDispatch, useSelector } from 'react-redux';
-import {
-  deleteChat,
-  deleteLastMessage,
-  getChats,
-  getMoreChats,
-  selectChats,
-  setLastMessage,
-  setNameFilter,
-  updateLastMessage,
-} from '../features/chats/chatsSlice';
-import { useCallback, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectChats } from '../features/chats/chatsSlice';
 import ChatsTable from '../features/chats/ChatsTable';
 import PageLoader from '../components/PageLoader';
 import { QueryStatusEnum } from '../enums/queryStatus.enum';
@@ -25,14 +15,7 @@ import PageError from '../components/PageError';
 import { LinearProgressPlaceholder } from '../components/LinearProgressPlaceholder';
 import LoadMoreBtn from '../components/LoadMoreBtn';
 import { ResponsiveStyleValue } from '@mui/system';
-import useDeleteChatListener from '../features/chats/useDeleteChatListener';
-import {
-  IDeleteMessageEventPayload,
-  IMessageEventPayload,
-} from '../types/message.interfaces';
-import useNewMessageListener from '../features/chats/useNewMessageListener';
-import useUpdateMessageListener from '../features/chats/useUpdateMessageListener';
-import useDeleteMessageListener from '../features/chats/useDeleteMessageListener';
+import useChats from '../features/chats/useChats';
 
 const headerStyles: SxProps = {
   mb: 2,
@@ -45,54 +28,7 @@ const headerDirection: ResponsiveStyleValue<'row' | 'column'> = {
 
 export default function AllChatsPage() {
   const { chats, hasMore, status } = useSelector(selectChats);
-  const dispatch = useDispatch();
-
-  const handleDelete = useCallback(
-    (id: string) => {
-      dispatch(deleteChat(id));
-    },
-    [dispatch],
-  );
-  useDeleteChatListener(handleDelete);
-
-  const handleNewMessage = useCallback(
-    (message: IMessageEventPayload) => {
-      dispatch(setLastMessage(message));
-    },
-    [dispatch],
-  );
-  useNewMessageListener(handleNewMessage);
-
-  const handleEditMessage = useCallback(
-    (message: IMessageEventPayload) => {
-      dispatch(updateLastMessage(message));
-    },
-    [dispatch],
-  );
-  useUpdateMessageListener(handleEditMessage);
-
-  const handleDeleteMessage = useCallback(
-    (message: IDeleteMessageEventPayload) => {
-      dispatch(deleteLastMessage(message));
-    },
-    [dispatch],
-  );
-  useDeleteMessageListener(handleDeleteMessage);
-
-  const handleNameFilterChange = useCallback(
-    (name: string) => {
-      dispatch(setNameFilter(name));
-    },
-    [dispatch],
-  );
-
-  const handleLoadMore = useCallback(() => {
-    dispatch(getMoreChats());
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(getChats());
-  }, [dispatch]);
+  const { handleLoadMore, handleNameFilterChange } = useChats();
 
   return (
     <>
