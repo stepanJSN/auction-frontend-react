5de9b91f7f346895a85f90b01418f8783b8363e3
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-15 16:55:08 +0200

    extract business logic of chat page to custom hook

---
+++ src/features/chats/chat/useChat.ts
@@ -0,0 +1,111 @@
+import { useEffect, useCallback, useState } from 'react';
+import {
+  IMessageEventPayload,
+  IDeleteMessageEventPayload,
+} from '../../../types/message.interfaces';
+import useDeleteChatListener from '../useDeleteChatListener';
+import useDeleteMessageListener from '../useDeleteMessageListener';
+import useNewMessageListener from '../useNewMessageListener';
+import {
+  getChat,
+  createMessage,
+  getMoreMessages,
+  deleteMessage,
+  resendMessage,
+  addMessage,
+  setDeleteMessageSuccess,
+  selectChat,
+} from './chatSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAuth } from '../../auth/authSlice';
+
+export default function useChat(chatId?: string) {
+  const dispatch = useDispatch();
+  const { id } = useSelector(selectAuth);
+  const { messages } = useSelector(selectChat);
+  const [isScrollToBottomActive, setIsScrollToBottomActive] = useState(true);
+  const [isChatDeletedDialogOpen, setIsChatDeletedDialogOpen] = useState(false);
+  useEffect(() => {
+    if (!chatId) return;
+    dispatch(getChat(chatId));
+  }, [dispatch, chatId]);
+
+  const handleCreateMessage = useCallback(
+    (data: { message: string }) => {
+      if (!chatId) return;
+      dispatch(
+        createMessage({
+          tempId: new Date().getTime().toString(),
+          chatId,
+          message: data.message,
+        }),
+      );
+      setIsScrollToBottomActive(true);
+    },
+    [chatId, dispatch],
+  );
+
+  const handleLoadMore = useCallback(() => {
+    if (!chatId || !messages.hasNextPage) return;
+    dispatch(getMoreMessages(chatId));
+    setIsScrollToBottomActive(false);
+  }, [chatId, dispatch, messages.hasNextPage]);
+
+  const handleMessageDelete = useCallback(
+    (messageId: string) => {
+      if (!chatId) return;
+      dispatch(
+        deleteMessage({
+          messageId,
+          chatId,
+        }),
+      );
+    },
+    [chatId, dispatch],
+  );
+
+  const handleMessageResend = useCallback(
+    (messageId: string) => {
+      if (!chatId) return;
+      dispatch(
+        resendMessage({
+          tempId: messageId,
+          chatId,
+        }),
+      );
+    },
+    [chatId, dispatch],
+  );
+
+  const handleNewMessage = useCallback(
+    (message: IMessageEventPayload) => {
+      if (message.chat_id !== chatId || message.sender.id === id) return;
+      dispatch(addMessage(message));
+      setIsScrollToBottomActive(true);
+    },
+    [chatId, dispatch, id],
+  );
+  useNewMessageListener(handleNewMessage);
+
+  const handleDeleteMessageEvent = useCallback(
+    (data: IDeleteMessageEventPayload) => {
+      dispatch(setDeleteMessageSuccess(data.id));
+    },
+    [dispatch],
+  );
+  useDeleteMessageListener(handleDeleteMessageEvent);
+
+  const handleDeleteChatEvent = useCallback((_id: string) => {
+    setIsChatDeletedDialogOpen(true);
+  }, []);
+  useDeleteChatListener(handleDeleteChatEvent);
+
+  return {
+    handleCreateMessage,
+    handleLoadMore,
+    handleMessageDelete,
+    handleMessageResend,
+    isScrollToBottomActive,
+    isChatDeletedDialogOpen,
+  };
+}
--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,32 +1,15 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
-import {
-  addMessage,
-  createMessage,
-  deleteMessage,
-  getChat,
-  getMoreMessages,
-  resendMessage,
-  selectChat,
-  setDeleteMessageSuccess,
-} from '../features/chats/chat/chatSlice';
+import { selectChat } from '../features/chats/chat/chatSlice';
 import { Divider, Grid2 } from '@mui/material';
 import ChatHeader from '../features/chats/chat/ChatHeader';
 import { QueryStatusEnum } from '../enums/queryStatus.enum';
 import ChatField from '../features/chats/chat/ChatField';
-import { useCallback, useEffect, useState } from 'react';
 import ChatSettings from '../features/chats/chat/ChatSettings';
 import MessageForm from '../features/chats/chat/MessageForm';
-import useNewMessageListener from '../features/chats/useNewMessageListener';
-import {
-  IDeleteMessageEventPayload,
-  IMessageEventPayload,
-} from '../types/message.interfaces';
-import { selectAuth } from '../features/auth/authSlice';
-import useDeleteMessageListener from '../features/chats/useDeleteMessageListener';
-import useDeleteChatListener from '../features/chats/useDeleteChatListener';
 import ChatDeletedDialog from '../features/chats/chat/ChatDeletedDialog';
 import useSidebarVisibility from '../hooks/useSidebarVisibility';
+import useChat from '../features/chats/chat/useChat';
 
 const settingsGridBreakpoints = {
   xs: 0,
@@ -35,11 +18,7 @@ const settingsGridBreakpoints = {
 
 export default function ChatPage() {
   const { chatId } = useParams();
-  const dispatch = useDispatch();
   const { name, participants, status, messages } = useSelector(selectChat);
-  const { id } = useSelector(selectAuth);
-  const [isScrollToBottomActive, setIsScrollToBottomActive] = useState(true);
-  const [isChatDeletedDialogOpen, setIsChatDeletedDialogOpen] = useState(false);
   const {
     isSidebarOpen,
     isMobileVersion,
@@ -47,81 +26,14 @@ export default function ChatPage() {
     handleSidebarClose,
     ref,
   } = useSidebarVisibility('md');
-
-  useEffect(() => {
-    if (!chatId) return;
-    dispatch(getChat(chatId));
-  }, [dispatch, chatId]);
-
-  const handleSubmit = useCallback(
-    (data: { message: string }) => {
-      if (!chatId) return;
-      dispatch(
-        createMessage({
-          tempId: new Date().getTime().toString(),
-          chatId,
-          message: data.message,
-        }),
-      );
-      setIsScrollToBottomActive(true);
-    },
-    [chatId, dispatch],
-  );
-
-  const handleLoadMore = useCallback(() => {
-    if (!chatId || !messages.hasNextPage) return;
-    dispatch(getMoreMessages(chatId));
-    setIsScrollToBottomActive(false);
-  }, [chatId, dispatch, messages.hasNextPage]);
-
-  const handleMessageDelete = useCallback(
-    (messageId: string) => {
-      if (!chatId) return;
-      dispatch(
-        deleteMessage({
-          messageId,
-          chatId,
-        }),
-      );
-    },
-    [chatId, dispatch],
-  );
-
-  const handleMessageResend = useCallback(
-    (messageId: string) => {
-      if (!chatId) return;
-      dispatch(
-        resendMessage({
-          tempId: messageId,
-          chatId,
-        }),
-      );
-    },
-    [chatId, dispatch],
-  );
-
-  const handleNewMessage = useCallback(
-    (message: IMessageEventPayload) => {
-      if (message.chat_id !== chatId || message.sender.id === id) return;
-      dispatch(addMessage(message));
-      setIsScrollToBottomActive(true);
-    },
-    [chatId, dispatch, id],
-  );
-  useNewMessageListener(handleNewMessage);
-
-  const handleDeleteMessageEvent = useCallback(
-    (data: IDeleteMessageEventPayload) => {
-      dispatch(setDeleteMessageSuccess(data.id));
-    },
-    [dispatch],
-  );
-  useDeleteMessageListener(handleDeleteMessageEvent);
-
-  const handleDeleteChatEvent = useCallback((_id: string) => {
-    setIsChatDeletedDialogOpen(true);
-  }, []);
-  useDeleteChatListener(handleDeleteChatEvent);
+  const {
+    handleLoadMore,
+    handleCreateMessage,
+    handleMessageDelete,
+    handleMessageResend,
+    isScrollToBottomActive,
+    isChatDeletedDialogOpen,
+  } = useChat();
 
   return (
     <>
@@ -142,7 +54,7 @@ export default function ChatPage() {
                 onResendMessage={handleMessageResend}
                 onLoadMoreMessages={handleLoadMore}
               />
-              <MessageForm onSubmit={handleSubmit} />
+              <MessageForm onSubmit={handleCreateMessage} />
             </>
           )}
         </Grid2>
