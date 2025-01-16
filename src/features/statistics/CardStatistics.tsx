7cb73d623779887871284719dd7945a762ee5c67
Stepan JSN <stepan.s.jsn@gmail.com>
2025-01-16 16:32:27 +0200

    General statistics UI

---
+++ src/features/statistics/CardStatistics.tsx
@@ -0,0 +1,49 @@
+import { Paper, SxProps, Typography } from '@mui/material';
+import { Link } from 'react-router';
+import { ROUTES } from '../../config/routesConfig';
+
+type CardStatisticsProps = {
+  id?: string;
+  name?: string;
+  label: string;
+  number: number;
+};
+
+const textStyles: SxProps = {
+  display: 'block',
+  fontWeight: 'bold',
+  textAlign: 'center',
+};
+const containerStyles: SxProps = {
+  width: 200,
+  height: 150,
+  p: 2,
+};
+
+export default function CardStatistics({
+  id,
+  name,
+  number,
+  label,
+}: CardStatisticsProps) {
+  return (
+    <Paper sx={containerStyles}>
+      <Typography sx={textStyles} gutterBottom>
+        {label}
+      </Typography>
+      {name && id && (
+        <Typography
+          sx={textStyles}
+          component={Link}
+          variant="h6"
+          gutterBottom
+          to={ROUTES.CARD_DETAILS(id)}>
+          {name}
+        </Typography>
+      )}
+      <Typography sx={textStyles} variant="h6">
+        {number}
+      </Typography>
+    </Paper>
+  );
+}
