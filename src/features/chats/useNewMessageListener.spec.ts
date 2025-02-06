922abe3b9bb95abd79ced63d03608140a866e23b
Stepan JSN <stepan.s.jsn@gmail.com>
2025-02-06 13:29:25 +0200

    remove unnecessary dispatch from useNewMessageListener and write tests for this hook

---
+++ src/features/chats/useNewMessageListener.spec.ts
@@ -0,0 +1,61 @@
+import { renderHook } from '@testing-library/react';
+import { socket } from '../../socket';
+import { ChatsEventEnum } from './chatsEventsEnum';
+import useNewMessageListener from './useNewMessageListener';
+
+jest.mock('../../socket', () => ({
+  socket: {
+    on: jest.fn(),
+    off: jest.fn(),
+  },
+}));
+
+describe('useNewMessageListener', () => {
+  const mockOnNewMessage = jest.fn();
+  const mockMessagePayload = {
+    id: 'messageId',
+    chat_id: 'chatId',
+    message: 'someMessage',
+    created_at: '2025-01-01T00:00:00.000Z',
+    sender: {
+      id: 'senderId',
+      name: 'name',
+      surname: 'surname',
+    },
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should subscribe to the NEW_MESSAGE event on mount', () => {
+    renderHook(() => useNewMessageListener(mockOnNewMessage));
+
+    expect(socket.on).toHaveBeenCalledWith(
+      ChatsEventEnum.NEW_MESSAGE,
+      mockOnNewMessage,
+    );
+  });
+
+  it('should call onCreate when a NEW_MESSAGE event is received', () => {
+    renderHook(() => useNewMessageListener(mockOnNewMessage));
+
+    const eventHandler = (socket.on as jest.Mock).mock.calls[0][1];
+    eventHandler(mockMessagePayload);
+
+    expect(mockOnNewMessage).toHaveBeenCalledWith(mockMessagePayload);
+  });
+
+  it('should unsubscribe from the NEW_MESSAGE event on unmount', () => {
+    const { unmount } = renderHook(() =>
+      useNewMessageListener(mockOnNewMessage),
+    );
+
+    unmount();
+
+    expect(socket.off).toHaveBeenCalledWith(
+      ChatsEventEnum.NEW_MESSAGE,
+      mockOnNewMessage,
+    );
+  });
+});
--- src/features/chats/useNewMessageListener.ts
+++ src/features/chats/useNewMessageListener.ts
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { socket } from '../../socket';
 import { ChatsEventEnum } from './chatsEventsEnum';
 import { IMessageEventPayload } from '../../types/message.interfaces';
@@ -7,12 +6,10 @@ import { IMessageEventPayload } from '../../types/message.interfaces';
 export default function useNewMessageListener(
   onNewMessage: (id: IMessageEventPayload) => void,
 ) {
-  const dispatch = useDispatch();
-
   useEffect(() => {
     socket.on(ChatsEventEnum.NEW_MESSAGE, onNewMessage);
     return () => {
       socket.off(ChatsEventEnum.NEW_MESSAGE, onNewMessage);
     };
-  }, [dispatch, onNewMessage]);
+  }, [onNewMessage]);
 }
